48383f3415e92049b9f89562ef84375bf91783b6
Trevor Howard <thatrevguy@gmail.com>
2020-01-24 01:06:33 -0700

    Update PreConfig.js

--- src/main/webapp/js/PreConfig.js
+++ src/main/webapp/js/PreConfig.js
@@ -7,3 +7,7 @@ window.EXPORT_URL = 'REPLACE_WITH_YOUR_IMAGE_SERVER';
 window.PLANT_URL = 'REPLACE_WITH_YOUR_PLANTUML_SERVER';
 window.DRAW_MATH_URL = 'math';
 window.DRAWIO_CONFIG = null; //Replace with your custom draw.io configurations. For more details, https://desk.draw.io/support/solutions/articles/16000058316
+
+// https://github.com/jgraph/drawio/issues/642
+window.DRAWIO_GOOGLE_VIEWER_APP_ID = '1012810279595-6cq19om13gb1tibv1qu2jpdcmeihbtqg';
+window.DRAWIO_GOOGLE_VIEWER_CLIENT_ID = '1012810279595-6cq19om13gb1tibv1qu2jpdcmeihbtqg.apps.googleusercontent.com';
